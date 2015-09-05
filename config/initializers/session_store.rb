fa217d622c4b0e540c4f332a5870a6cf3d442d84
Keiko <Keiko@kanazawayoshiko-no-MacBook-Air.local>
2015-09-06 04:17:43 +0900

    Finish static pages

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,17 +2,3 @@
 
 Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
 
-require 'securerandom'
-
-def secure_token
-	token_file = Rails.root.join('.secret')
-	if File.exist?(token_rile)
-		File.read(token_file).chomp
-	else
-		token = SecureRandom.hex(64)
-		File.write(token_file, token)
-		token
-	end
-end
-
-SampleApp::Application.config.secret_key_base = secure_token
